bf47ea0003b8b4e03b065d47d1f3f43f47e9ad9a
ByteStorm05 <143643985+ByteStorm05@users.noreply.github.com>
2024-10-25 21:41:28 +0530

    Commit for Submission v1.2

--- src/app/components/Work.tsx
+++ src/app/components/Work.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const cardData = [
   {
     title: "Real-time Stock Updates",
     description: "Get instant updates on stock prices to stay informed.",
     image: "/images/dashb.jpg",
-    buttonText: "Test Real-time Updates",
+    buttonText: "Try Dashboard",
+    link: "/dashboard" // Add the path you want this button to link to
   },
   {
     title: "Buy Stocks",
     description: "Purchase Stocks by fetching real prices!",
     image: "/images/buy.jpg",
-    buttonText: "Try Authentication",
+    buttonText: "Try Buy Shares feature",
+    link: "/buystocks" // Add the path here
   },
   {
     title: "Personal Profile",
     description: "Access your purchased stocks at a glance.",
     image: "/images/profile.jpg",
-    buttonText: "View Dashboard",
+    buttonText: "Visit Profile",
+    link: "/profile" // And here
   },
 ];
 
@@ -39,21 +43,22 @@ const Work: React.FC = () => {
             <Image
               src={card.image}
               alt={card.title}
-              width={400} // Set width according to your preference
-              height={250} // Set a fixed height to maintain consistency
-              layout="responsive" // This helps maintain the aspect ratio
-              className="object-cover" // Keep this class for object cover
+              width={400}
+              height={250}
+              layout="responsive"
+              className="object-cover"
             />
 
             {/* Card Content */}
-            <div className="flex-grow p-6 text-center"> {/* Use flex-grow to maintain equal height */}
+            <div className="flex-grow p-6 text-center">
               <h3 className="text-2xl font-semibold mb-4">{card.title}</h3>
               <p className="text-gray-600 mb-6">{card.description}</p>
 
-              {/* Test Feature Button */}
-              <button className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
-                {card.buttonText}
-              </button>
+              {/* Test Feature Button as Link */}
+              <Link href={card.link} className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+
+                  {card.buttonText}
+              </Link>
             </div>
           </div>
         ))}
